ad80836cf4e9ef00758bd074172cf88620749606
lvera182 <lvera182@gmail.com>
2021-03-26 21:26:41 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+// Tipos de datos
+let Nombre = `Leonel Albeyro Vera Gomez`; //String
+let Edad = 29; //number - intenger - int;
+let altura = 1.62; //Double - float
+let profesor = false; // Boleano
+let informacion = [Nombre, Edad, altura, profesor]; //Array
+console.log(informacion);
+// console.log(informacion[2]);
+let vaso = {
+	material : "pasta",
+	diametro :  50,
+	lleno : true
+}; // Object
+console.log(vaso);
+// console.log(baso.diametro);
